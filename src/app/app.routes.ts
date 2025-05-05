995fdc84d212ed0e9e147df2b8c5c845730d6fcc
aaminatk22 <tribak.amina@ensi.ma>
2025-05-05 17:21:05 +0100

    modified routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
 import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/reserver', pathMatch: 'full' },
   { path: 'reserver', component: ReservationFormComponent },
   { path: 'reservations', component: ReservationListComponent }
